75c53f1a2d065148c0ecc566bd4a92c416b1fb88
xpfxz <xpfxz@bk.ru>
2025-06-26 22:31:28 +0300

    settings

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.views import ProductView
+
+
+router = DefaultRouter()
+router.register(r'products', ProductView, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('api/', include(router.urls)),
 ]
