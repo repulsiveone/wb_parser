4568f197e798884970827d743528ac805d7ba5ef
xpfxz <xpfxz@bk.ru>
2025-06-26 14:05:28 +0300

    add card model

--- src/apps/models.py
+++ src/apps/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CardModel(models.Model):
+    name = models.CharField("название", max_length=300)
+    price = models.IntegerField()
+    discounter_price = models.IntegerField()
+    rating = models.IntegerField()
+    feedbacks = models.IntegerField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['price']),
+            models.Index(fields=['rating']),
+        ]
